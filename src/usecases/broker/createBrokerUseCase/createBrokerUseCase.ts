528f6d93096ae76d1e3a60c8250e3af34af14101
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-11-14 00:02:56 -0300

    feat: publish user to queue after create

--- src/usecases/broker/createBrokerUseCase/createBrokerUseCase.ts
+++ src/usecases/broker/createBrokerUseCase/createBrokerUseCase.ts
@@ -1,10 +1,13 @@
 import { Broker } from "src/domain/models/broker";
 import { BrokerRepository } from "src/domain/repositories/brokerRepository.interface";
 import { CreateBroker } from "src/infrastructure/controllers/broker/dtos/createBroker";
+import { UserCreatedPublishService } from "src/infrastructure/rabbitmq/publishers/user-created/user-create-publish.service";
+import { UserCreatedPublishModule } from "src/infrastructure/rabbitmq/publishers/user-created/user-created-publish.module";
 
 export class CreateBrokerUseCase {
     constructor(
-        private readonly brokerRepository: BrokerRepository
+        private readonly brokerRepository: BrokerRepository,
+        private readonly userCreatedPublish: UserCreatedPublishService
     ) { }
 
     async execute(
@@ -31,6 +34,8 @@ export class CreateBrokerUseCase {
         broker.account_status = account_status;
 
         const insertionResult = await this.brokerRepository.insert(broker);
+        this.userCreatedPublish.publish(insertionResult);
+
         return insertionResult;
     }
 }
