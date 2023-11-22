fb0ba993781744d0776216ccc6bf144b71645e65
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-11-21 23:06:42 -0300

    feat: add updated method

--- src/infrastructure/controllers/broker/broker.controller.ts
+++ src/infrastructure/controllers/broker/broker.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Inject, ParseIntPipe, Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Inject, ParseIntPipe, Patch, Post, Query } from "@nestjs/common";
 import { UseCaseProxy } from "src/infrastructure/usecases-proxy/usecases-proxy";
 import { UsecasesProxyModule } from "src/infrastructure/usecases-proxy/usecases-proxy.module";
 import { CreateBrokerUseCase } from "src/usecases/broker/createBrokerUseCase/createBrokerUseCase";
 import { FindBrokerUseCase } from "src/usecases/broker/findBrokerUseCase/findBrokerUseCase";
 import { CreateBroker } from "./dtos/createBroker";
+import { UpdateBroker } from "./dtos/updateBroker";
+import { UpdateBrokerUseCase } from "src/usecases/broker/updateBrokerUseCase/updateBrokerUseCase";
 
 @Controller('broker')
 export class BrokerController {
@@ -12,6 +14,8 @@ export class BrokerController {
         private readonly getBrokerUsecaseProxy: UseCaseProxy<FindBrokerUseCase>,
         @Inject(UsecasesProxyModule.CREATE_BROKER_USECASES_PROXY)
         private readonly createBrokerUsecaseProxy: UseCaseProxy<CreateBrokerUseCase>,
+        @Inject(UsecasesProxyModule.UPDATE_BROKER_USECASES_PROXY)
+        private readonly updateBrokerUsecaseProxy: UseCaseProxy<UpdateBrokerUseCase>,
       ) {}
     
     @Get('/:id')
@@ -30,5 +34,10 @@ export class BrokerController {
     @Post()
     async createBroker(@Body() broker: CreateBroker) {
         return await this.createBrokerUsecaseProxy.getInstance().execute(broker); 
+    }
+
+    @Patch()
+    async updateBroker(@Body() broker: UpdateBroker) {
+        return await this.updateBrokerUsecaseProxy.getInstance().execute(broker); 
     }
 }
