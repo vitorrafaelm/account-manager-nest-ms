4ef5e1b043e716c211ca2a74a5381723d5845b0f
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-11-21 23:11:59 -0300

    feat: import update publish module

--- src/infrastructure/rabbitmq/rabbitmq.module.ts
+++ src/infrastructure/rabbitmq/rabbitmq.module.ts
@@ -1,11 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { UserCreatedPublishModule } from './publishers/user-created/user-created-publish.module';
+import { UserUpdatedPublishModule } from './publishers/user-updated/user-updated-publish.module';
 
 @Global()
 @Module({
     imports: [
-        UserCreatedPublishModule
+        UserCreatedPublishModule, 
+        UserUpdatedPublishModule
     ], 
-    exports: [UserCreatedPublishModule]
+    exports: [UserCreatedPublishModule, UserUpdatedPublishModule]
 })
 export class RabbitmqModule {}
