737824fe7cd65816cbf2fa876394ce0b304f3be5
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-12-04 21:26:09 -0300

    fix: add cryptography

--- src/usecases/broker/createBrokerUseCase/createBrokerUseCase.ts
+++ src/usecases/broker/createBrokerUseCase/createBrokerUseCase.ts
@@ -1,12 +1,14 @@
 import { Broker } from "src/domain/models/broker";
 import { BrokerRepository } from "src/domain/repositories/brokerRepository.interface";
+import { CryptoService } from "src/infrastructure/commom/utils/crypto/crypto.service";
 import { CreateBroker } from "src/infrastructure/controllers/broker/dtos/createBroker";
 import { UserCreatedPublishService } from "src/infrastructure/rabbitmq/publishers/user-created/user-create-publish.service";
 
 export class CreateBrokerUseCase {
     constructor(
         private readonly brokerRepository: BrokerRepository,
-        private readonly userCreatedPublish: UserCreatedPublishService
+        private readonly userCreatedPublish: UserCreatedPublishService,
+        private readonly cryptoSerive: CryptoService
     ) { }
 
     async execute(
@@ -16,24 +18,29 @@ export class CreateBrokerUseCase {
             password,
             document_type,
             document_number,
-            broker_identifier,
+            identifier,
             profile_picture,
             account_status, 
             address
         }: CreateBroker
     ): Promise<Broker> {
         const broker = new Broker();
+        
+        const { encryptedData, initVector } = await this.cryptoSerive.encrypt(password); 
 
         broker.name = name;
         broker.email = email;
-        broker.password = password;
+        broker.password = encryptedData;
         broker.document_type = document_type;
         broker.document_number = document_number;
-        broker.broker_identifier = broker_identifier;
+        broker.identifier = identifier;
         broker.profile_picture = profile_picture;
         broker.account_status = account_status;
+        broker.iv = initVector.toString();
 
-        broker.address = address;
+        console.log(address, 'address')
+        broker.addresses = address;
+        broker.addresses.broker_identifier = identifier;
 
         const insertionResult = await this.brokerRepository.insert(broker);
         this.userCreatedPublish.publish(insertionResult);
