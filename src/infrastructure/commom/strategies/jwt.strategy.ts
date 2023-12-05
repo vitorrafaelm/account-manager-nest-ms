bc5865d815c5b6da0e86a472a095ac5d0aaeeae7
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-12-04 21:34:25 -0300

    feat: create jwt strategy

---
+++ src/infrastructure/commom/strategies/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    if (!payload.email || !payload.identifier) {
+      throw new Error('JWT STRATEGY: Email or identifier is missing, BadRequestException');
+    }
+
+    return { ...payload };
+  }
+}
