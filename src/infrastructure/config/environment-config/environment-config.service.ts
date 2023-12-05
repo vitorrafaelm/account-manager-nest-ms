975508c3c391f53117cfad34f58e881eb2d12433
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-12-04 21:36:13 -0300

    feat: add new fundtions to get .env values

--- src/infrastructure/config/environment-config/environment-config.service.ts
+++ src/infrastructure/config/environment-config/environment-config.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DatabaseConfig } from 'src/domain/config/DatabaseConfig';
+import { JWTConfig } from 'src/domain/config/jwt.interface';
 
 @Injectable()
-export class EnvironmentConfigService implements DatabaseConfig {
-    constructor(private configService: ConfigService) {}
+export class EnvironmentConfigService implements DatabaseConfig, JWTConfig {
+    constructor(private configService: ConfigService) { }
+
+    getInitVectorKey(): string {
+        return this.configService.get<string>('INIT_VECTOR');
+    }
 
     getDatabaseHost(): string {
         return this.configService.get<string>('DATABASE_HOST');
@@ -26,5 +31,16 @@ export class EnvironmentConfigService implements DatabaseConfig {
     }
     getDatabaseSync(): boolean {
         return this.configService.get<boolean>('DATABASE_SYNCHRONIZE');
+    }
+    getEncryptKey(): string {
+        return this.configService.get<string>('ENCRYPT_KEY');
+    }
+
+    getJwtSecret(): string {
+        return this.configService.get<string>('JWT_SECRET');
+    }
+
+    getJwtExpirationTime(): string {
+        return this.configService.get<string>('JWT_EXPIRATION_TIME');
     }
 }
