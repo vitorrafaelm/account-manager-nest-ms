49d3530de76eef7a86aa444218699674e30c2324
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-12-04 21:36:45 -0300

    feat: add proxyies for usecases created

--- src/infrastructure/usecases-proxy/usecases-proxy.module.ts
+++ src/infrastructure/usecases-proxy/usecases-proxy.module.ts
@@ -11,15 +11,25 @@ import { RabbitmqModule } from '../rabbitmq/rabbitmq.module';
 import { UserCreatedPublishService } from '../rabbitmq/publishers/user-created/user-create-publish.service';
 import { UserUpdatedPublishService } from '../rabbitmq/publishers/user-updated/user-update-publish.service';
 import { UpdateBrokerUseCase } from 'src/usecases/broker/updateBrokerUseCase/updateBrokerUseCase';
+import { CryptoService } from '../commom/utils/crypto/crypto.service';
+import { CryptoModule } from '../commom/utils/crypto/crypto.module';
+import { LoginBrokerUseCase } from 'src/usecases/broker/loginBrokerUseCase/loginBrokerUseCase';
+import { IJwtService } from 'src/domain/adapters/jwt.interface';
+import { EnvironmentConfigService } from '../config/environment-config/environment-config.service';
+import { JwtTokenService } from '../services/jwt/jwt.service';
+import { JwtModule } from '../services/jwt/jwt.module';
+import { SoftDeleteBrokerUseCase } from 'src/usecases/broker/softDeleteBrokerUseCase/softDeleteBrokerUseCase';
 
 
 @Module({
-  imports: [EnvironmentConfigModule, RepositoriesModule, RabbitmqModule],
+  imports: [EnvironmentConfigModule, RepositoriesModule, RabbitmqModule, CryptoModule, JwtModule],
 })
 export class UsecasesProxyModule {
   static GET_BROKER_USECASES_PROXY = 'getBrokerUsecasesProxy';
   static CREATE_BROKER_USECASES_PROXY = 'createBrokerUsecasesProxy';
   static UPDATE_BROKER_USECASES_PROXY = 'updateBrokerUsecasesProxy';
+  static LOGIN_BROKER_USECASES_PROXY = 'loginBrokerUsecasesProxy';
+  static SOFT_DELETE_BROKER_USECASES_PROXY = 'softDeleteBrokerUsecasesProxy'
 
   static register(): DynamicModule {
     return {
@@ -31,20 +41,32 @@ export class UsecasesProxyModule {
           useFactory: (brokerRepository: DatabaseBrokerRepository) => new UseCaseProxy(new FindBrokerUseCase(brokerRepository)),
         },
         {
-          inject: [DatabaseBrokerRepository, UserCreatedPublishService],
+          inject: [DatabaseBrokerRepository, UserCreatedPublishService, CryptoService],
           provide: UsecasesProxyModule.CREATE_BROKER_USECASES_PROXY,
-          useFactory: (brokerRepository: DatabaseBrokerRepository, userCreatedPublish: UserCreatedPublishService) => new UseCaseProxy(new CreateBrokerUseCase(brokerRepository, userCreatedPublish)),
+          useFactory: (brokerRepository: DatabaseBrokerRepository, userCreatedPublish: UserCreatedPublishService, cryptoService: CryptoService) => new UseCaseProxy(new CreateBrokerUseCase(brokerRepository, userCreatedPublish, cryptoService)),
+        }, 
+        {
+          inject: [DatabaseBrokerRepository, UserUpdatedPublishService, CryptoService],
+          provide: UsecasesProxyModule.UPDATE_BROKER_USECASES_PROXY,
+          useFactory: (brokerRepository: DatabaseBrokerRepository, userUpdatePublish: UserUpdatedPublishService, cryptoService: CryptoService) => new UseCaseProxy(new UpdateBrokerUseCase(brokerRepository, userUpdatePublish, cryptoService)),
+        }, 
+        {
+          inject: [DatabaseBrokerRepository, JwtTokenService, EnvironmentConfigService, CryptoService],
+          provide: UsecasesProxyModule.LOGIN_BROKER_USECASES_PROXY,
+          useFactory: (brokerRepository: DatabaseBrokerRepository, JwtTokenService: IJwtService, jwtConfig: EnvironmentConfigService, cryptoService: CryptoService) => new UseCaseProxy(new LoginBrokerUseCase(brokerRepository, JwtTokenService, jwtConfig, cryptoService)),
         }, 
         {
           inject: [DatabaseBrokerRepository, UserUpdatedPublishService],
-          provide: UsecasesProxyModule.UPDATE_BROKER_USECASES_PROXY,
-          useFactory: (brokerRepository: DatabaseBrokerRepository, userUpdatePublish: UserUpdatedPublishService) => new UseCaseProxy(new UpdateBrokerUseCase(brokerRepository, userUpdatePublish)),
+          provide: UsecasesProxyModule.SOFT_DELETE_BROKER_USECASES_PROXY,
+          useFactory: (brokerRepository: DatabaseBrokerRepository, userUpdatePublish: UserUpdatedPublishService) => new UseCaseProxy(new SoftDeleteBrokerUseCase(brokerRepository, userUpdatePublish)),
         }
       ],
       exports: [
         UsecasesProxyModule.GET_BROKER_USECASES_PROXY,
         UsecasesProxyModule.CREATE_BROKER_USECASES_PROXY,
-        UsecasesProxyModule.UPDATE_BROKER_USECASES_PROXY
+        UsecasesProxyModule.UPDATE_BROKER_USECASES_PROXY, 
+        UsecasesProxyModule.LOGIN_BROKER_USECASES_PROXY,
+        UsecasesProxyModule.SOFT_DELETE_BROKER_USECASES_PROXY,
       ],
     };
   }
