93dc0a0e6e08dc1a501f4d58c207d8852f16d4ca
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-12-04 21:26:40 -0300

    fix: find broker by informations passed on jwt

--- src/usecases/broker/findBrokerUseCase/findBrokerUseCase.ts
+++ src/usecases/broker/findBrokerUseCase/findBrokerUseCase.ts
@@ -1,11 +1,16 @@
 import { BrokerRepository } from "src/domain/repositories/brokerRepository.interface";
 
+interface IPayload {
+    email: string; 
+    identifier: string
+}
+
 export class FindBrokerUseCase {
     constructor(
         private readonly brokerRepository: BrokerRepository
     ) {}
 
-    async execute(brokerId: number) {
-        return await this.brokerRepository.get(brokerId);
+    async execute(payload: IPayload) {
+        return await this.brokerRepository.getUserByIdentifier(payload.identifier)
     }
 }
