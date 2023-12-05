18cd88cf742d9c157618e7fc1a42cb93889bdb18
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-12-04 21:35:04 -0300

    feat: refactor broker controller and implement methods

--- src/infrastructure/controllers/broker/broker.controller.ts
+++ src/infrastructure/controllers/broker/broker.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Inject, ParseIntPipe, Patch, Post, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Inject, Patch, Post, Req, Request, Response, UseGuards } from "@nestjs/common";
 import { UseCaseProxy } from "src/infrastructure/usecases-proxy/usecases-proxy";
 import { UsecasesProxyModule } from "src/infrastructure/usecases-proxy/usecases-proxy.module";
 import { CreateBrokerUseCase } from "src/usecases/broker/createBrokerUseCase/createBrokerUseCase";
@@ -6,6 +6,10 @@ import { FindBrokerUseCase } from "src/usecases/broker/findBrokerUseCase/findBro
 import { CreateBroker } from "./dtos/createBroker";
 import { UpdateBroker } from "./dtos/updateBroker";
 import { UpdateBrokerUseCase } from "src/usecases/broker/updateBrokerUseCase/updateBrokerUseCase";
+import { AuthLoginDto } from "./dtos/login";
+import { LoginBrokerUseCase } from "src/usecases/broker/loginBrokerUseCase/loginBrokerUseCase";
+import { JwtAuthGuard } from "src/infrastructure/commom/guards/jwtAuth.guard";
+import { SoftDeleteBrokerUseCase } from "src/usecases/broker/softDeleteBrokerUseCase/softDeleteBrokerUseCase";
 
 @Controller('broker')
 export class BrokerController {
@@ -16,11 +20,40 @@ export class BrokerController {
         private readonly createBrokerUsecaseProxy: UseCaseProxy<CreateBrokerUseCase>,
         @Inject(UsecasesProxyModule.UPDATE_BROKER_USECASES_PROXY)
         private readonly updateBrokerUsecaseProxy: UseCaseProxy<UpdateBrokerUseCase>,
+        @Inject(UsecasesProxyModule.LOGIN_BROKER_USECASES_PROXY)
+        private readonly loginBrokerUsecasesProxy: UseCaseProxy<LoginBrokerUseCase>,
+        @Inject(UsecasesProxyModule.SOFT_DELETE_BROKER_USECASES_PROXY)
+        private readonly softDeleteBrokerUsecasesProxy: UseCaseProxy<SoftDeleteBrokerUseCase>,
       ) {}
+
+    @Post('/login')
+    async login(@Body() loginDTO: AuthLoginDto, @Request() request: any) {
+        try {
+            const access_informations = await this.loginBrokerUsecasesProxy.getInstance().execute(loginDTO.email, loginDTO.password); 
+            request.res.setHeader('Set-Cookie', [access_informations.cookie]);
+            request.res.statusCode = 200
+
+            return access_informations;
+        } catch (error) {
+            request.res.statusCode = 400
+            return {
+                error: '400', 
+                message: error.message
+            }
+        }
+
+        
+    }
+
+    @Post('/logout')
+    async logout(@Body() broker: CreateBroker) {
+        return await this.createBrokerUsecaseProxy.getInstance().execute(broker); 
+    }
     
-    @Get('/:id')
-    async getBroker(@Query('id') id: number) {
-        const broker = await this.getBrokerUsecaseProxy.getInstance().execute(id); 
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    async getBroker(@Req() request) {
+        const broker = await this.getBrokerUsecaseProxy.getInstance().execute(request.user); 
         
         if (!broker) {
             return {
@@ -37,7 +70,14 @@ export class BrokerController {
     }
 
     @Patch()
-    async updateBroker(@Body() broker: UpdateBroker) {
-        return await this.updateBrokerUsecaseProxy.getInstance().execute(broker); 
+    @UseGuards(JwtAuthGuard)
+    async updateBroker(@Body() broker: UpdateBroker, @Req() request) {
+        return await this.updateBrokerUsecaseProxy.getInstance().execute(broker, request.user); 
+    }
+
+    @Delete()
+    @UseGuards(JwtAuthGuard)
+    async deleteBroker(@Req() request) {
+        return await this.softDeleteBrokerUsecasesProxy.getInstance().execute(request.user); 
     }
 }
