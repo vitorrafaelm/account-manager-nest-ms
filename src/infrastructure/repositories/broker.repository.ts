5c6b57d2a133438044b397a68f420282a73ff85e
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-12-04 21:30:56 -0300

    fix: refactor repository broker

--- src/infrastructure/repositories/broker.repository.ts
+++ src/infrastructure/repositories/broker.repository.ts
@@ -11,21 +11,38 @@ export class DatabaseBrokerRepository implements BrokerRepository {
     constructor(
         @InjectRepository(Broker)
         private readonly brokerEntityRepository: Repository<Broker>
-    ) {}
+    ) { }
+
+    async getUserByEmail(email: string): Promise<Broker> {
+        return await this.brokerEntityRepository.findOne({ where: { email }, relations: { addresses: true } });
+    }
+
+    async getUserByIdentifier(identifier: string): Promise<Broker> {
+        return await this.brokerEntityRepository.findOne({ where: { identifier }, relations: { addresses: true } });
+    }
 
     async get(brokerId: number): Promise<Broker> {
-        return await this.brokerEntityRepository.findOne({ where: { id: brokerId }, relations: { address: true } }); 
+        return await this.brokerEntityRepository.findOne({ where: { id: brokerId }, relations: { addresses: true } });
     }
 
     async insert(broker: Broker): Promise<Broker> {
         const brokerCreated = this.brokerEntityRepository.create(broker);
-
+        console.log(brokerCreated)
         const brokerInserted = await this.brokerEntityRepository.save(brokerCreated);
         return this.brokerToEntity(brokerInserted);
     }
 
     async update(broker: Broker): Promise<Broker> {
         const brokerUpdated = await this.brokerEntityRepository.save({ id: broker.id, ...broker });
+        return brokerUpdated;
+    }
+
+    async softDelete(broker: Broker): Promise<Broker> {
+        const brokerUpdated = await this.brokerEntityRepository.save({ 
+            id: broker.id, 
+            is_deleted: broker.is_deleted, 
+            account_status: broker.account_status,
+            addresses: { is_deleted: broker.addresses.is_deleted, ...broker }});
         return brokerUpdated;
     }
 
@@ -42,15 +59,15 @@ export class DatabaseBrokerRepository implements BrokerRepository {
         broker.password = broker_inserted.password;
         broker.document_type = broker_inserted.document_type;
         broker.document_number = broker_inserted.document_number;
-        broker.broker_identifier = broker_inserted.broker_identifier;
+        broker.identifier = broker_inserted.identifier;
         broker.profile_picture = broker_inserted?.profile_picture;
         broker.account_status = broker_inserted.account_status;
-        broker.createdAt = broker_inserted?.createdAt; 
+        broker.createdAt = broker_inserted?.createdAt;
         broker.updatedAt = broker_inserted?.updatedAt;
 
-        broker.address = broker_inserted.address;
+        broker.addresses = broker_inserted.addresses;
 
         return broker;
     }
-    
+
 }
